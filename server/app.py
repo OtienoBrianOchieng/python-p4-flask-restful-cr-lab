3af552d944b43ca561bdd35e62a82cfa89222f92
root <root@DESKTOP-UEO62DE.localdomain>
2024-01-12 20:11:12 +0300

    Complete CRUD lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,44 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get_all_plants(self):
+        plants_list = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(
+        jsonify(plants_list),
+        200,
+        )
+        return response
+    def post_plant(self):
+        new_plant = Plant(
+            name = request.form['name'],
+            image = request.form['image'],
+            price = request.form['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        new_plant_dict = new_plant.to_dict()
+        response = make_response(
+            jsonify(new_plant_dict),
+            200,
+        )
+        response.headers['Content-Type'] = 'application/json'
+    
+        return response
+
 
 class PlantByID(Resource):
+    def get_plants_by_id(self, id):
+        plant_by_id = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(
+            jsonify(plant_by_id),
+            200,
+        )
+        return response
+    
     pass
         
-
+app.add_resource(Plants, '/plants')
+app.add_resource(PlantByID, '/plants/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
